08936ce4ebcce21ec775cc36cd0fe1ce26f37b4f
Md Adil <khanmdadil063@gmail.com>
2025-07-03 19:07:43 +0530

    crud mongoose

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const http = require("http");
-http
-  .createServer((req, resp) => {
-    resp.write("<h1>Hello MD d ssss ddddd</h1>");
+const mongoose = require("mongoose");
+const express = require("express");
+const product = require("./product");
+const { dbConnect } = require("./config");
+const app = express();
+app.use(express.json());
+require("dotenv").config();
 
-    resp.end();
-  })
-  .listen(7786);
+dbConnect().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+});
+app.post("/create", async (req, resp) => {
+  let data = new product(req.body);
+  let result = await data.save();
+
+  resp.send(result);
+});
+app.get("/list", async (req, resp) => {
+  let data = await product.find();
+
+  resp.send(data);
+});
+app.delete("/delete/:_id", async (req, resp) => {
+  let data = await product.deleteOne(req.params);
+
+  resp.send(data);
+});
+
+app.put("/update/:_id", async (req, resp) => {
+  console.log(req.params);
+  let data = await product.updateOne(req.params, {
+    $set: req.body,
+  });
+  console.log(req.body);
+  resp.send(data);
+});
