798f0cd8af051fdd0607337a637836736c778155
Md Adil <khanmdadil063@gmail.com>
2025-07-04 12:28:08 +0530

    multiple search api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const product = require("./product");
+const Product = require("./product");
 const { dbConnect } = require("./config");
 const app = express();
 app.use(express.json());
@@ -11,28 +11,10 @@ dbConnect().then(() => {
     console.log(`Server is running on port ${process.env.PORT}`);
   });
 });
-app.post("/create", async (req, resp) => {
-  let data = new product(req.body);
-  let result = await data.save();
-
-  resp.send(result);
-});
-app.get("/list", async (req, resp) => {
-  let data = await product.find();
-
-  resp.send(data);
-});
-app.delete("/delete/:_id", async (req, resp) => {
-  let data = await product.deleteOne(req.params);
-
-  resp.send(data);
-});
-
-app.put("/update/:_id", async (req, resp) => {
-  console.log(req.params);
-  let data = await product.updateOne(req.params, {
-    $set: req.body,
+app.get("/search/:key", async (req, resp) => {
+  let data = await Product.find({
+    $or: [{ name: { $regex: req.params.key } }, { brand: { $regex: req.params.key } }],
   });
-  console.log(req.body);
+  console.log(req.params);
   resp.send(data);
 });
