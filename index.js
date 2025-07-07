bd61ebd50417bbd210a079426fda1c3637b9a0f7
Md Adil <khanmdadil063@gmail.com>
2025-07-07 13:18:46 +0530

    file uploaded

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const Product = require("./product");
+// const Product = require("./product");
 const { dbConnect } = require("./config");
+const multer = require("multer");
 const app = express();
 app.use(express.json());
 require("dotenv").config();
@@ -11,10 +12,17 @@ dbConnect().then(() => {
     console.log(`Server is running on port ${process.env.PORT}`);
   });
 });
-app.get("/search/:key", async (req, resp) => {
-  let data = await Product.find({
-    $or: [{ name: { $regex: req.params.key } }, { brand: { $regex: req.params.key } }],
-  });
-  console.log(req.params);
-  resp.send(data);
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now() + ".jpg");
+    },
+  }),
+}).single("user_file");
+
+app.post("/upload", upload, (req, resp) => {
+  resp.send("file uploaded");
 });
